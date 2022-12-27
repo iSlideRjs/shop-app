4701f4ecebdb9af5fabedd83c4e59854e257c8f4
iKHVRV <opanlol@mail.ru>
2022-12-27 20:32:14 +0300

    Add Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
 function Header() {
-  return <div></div>;
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">
+          {/* <img
+            alt=""
+            src=""
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '} */}
+          Shop App
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 }
 
 export { Header };
