c6ccf5175ae4c02a9e8d92d2c9fb4279fee95823
iKHVRV <opanlol@mail.ru>
2022-12-27 20:21:51 +0300

    Connecting React-Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Header } from './components/Header';
+import { Shop } from './components/Shop';
+import { Footer } from './components/Footer';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Shop />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
